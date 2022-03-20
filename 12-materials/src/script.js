215de6ef8bbd86e78a0e5c60fe62d8727237384b
James Dempsey <jameswdemps@gmail.com>
2022-03-20 12:10:11 +0000

    finished lesson 12

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -1,12 +1,19 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { MeshBasicMaterial } from 'three'
+import { CubeTextureLoader, MeshBasicMaterial } from 'three'
+import * as dat from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ closed: true, width: 400 })
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
@@ -17,6 +24,18 @@ const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
 const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+gradientTexture.minFilter = THREE.NearestFilter
+gradientTexture.magFilter = THREE.NearestFilter
+gradientTexture.generateMipmaps = false
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg',
+])
 
 /**
  * Base
@@ -47,7 +66,39 @@ const scene = new THREE.Scene()
 
 // const material = new THREE.MeshDepthMaterial()
 
-const material = new THREE.MeshLambertMaterial()
+// const material = new THREE.MeshLambertMaterial()
+
+// const material = new THREE.MeshPhongMaterial()
+
+// material.shininess = 1000
+// material.specular = new THREE.Color(0x1188ff)
+
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientTexture
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.45
+material.roughness = 0.65
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.1
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+material.envMap = environmentMapTexture
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(10).step(0.0001)
+
+
 
 /**
  * Lights
@@ -63,22 +114,48 @@ pointLight.position.z = 1
 scene.add(pointLight)
 
 const sphere = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.SphereBufferGeometry(0.5, 64, 64),
     material
 )
 sphere.position.x = - 1.5
 
+
+sphere.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(
+        sphere.geometry.attributes.uv.array,
+        2
+    )
+)
+
 const plane = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(1,1),
+    new THREE.PlaneBufferGeometry(1,1, 100, 100),
     material
 )
 
+plane.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(
+        plane.geometry.attributes.uv.array,
+        2
+    )
+)
+
 const torus = new THREE.Mesh(
-    new THREE.TorusBufferGeometry(0.3, 0.2, 16, 32),
+    new THREE.TorusBufferGeometry(0.3, 0.2, 64, 128),
     material
 )
 
 torus.position.x = 1.5
+
+torus.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(
+        torus.geometry.attributes.uv.array,
+        2
+    )
+)
+
 
 scene.add(sphere, plane, torus)
 
